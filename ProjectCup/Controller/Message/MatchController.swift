b80d80c2e0ff84334c5a1c0e0bed647d7d1a8e99
younith <amber@butv10.com>
2019-04-01 19:56:44 -0400

    4/1 Match table gone

--- ProjectCup/Controller/Message/MatchController.swift
+++ ProjectCup/Controller/Message/MatchController.swift
@@ -9,21 +9,26 @@
 import UIKit
 import Firebase
 
-class MatchController: UITableViewController {
+class MatchController: UIViewController {
     
     let cellId = "cellId"
     var users = [User]()
     var randUser = [User]()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         
-        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
-        
         fetchUser()
+        
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(matchNameLabel)
+        view.addSubview(ContinueButton)
+        
+        setupMatchNameLabel()
+        setupContinueButton()
     }
     
     
@@ -47,11 +52,12 @@ class MatchController: UITableViewController {
                         user.id = snapshot.key
                         if user.id == userMatch && user.id != uid {
                             self.users.append(user)
+                            
+                            // Fix later for efficiency
+                            self.randUser.removeAll()
+                            let tempUser = self.users.randomElement()!
+                            self.randUser.append(tempUser)
                         }
-                        //this will crash because of background thread, so lets use dispatch_async to fix
-                        DispatchQueue.main.async(execute: {
-                            self.tableView.reloadData()
-                        })
                     }
                 }, withCancel: nil)
             }, withCancel: nil)
@@ -64,37 +70,57 @@ class MatchController: UITableViewController {
     }
     
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
-    }
+    let matchNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "We found a match for you"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
     
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserCell
+    var ContinueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r: 171, g: 234, b: 190)
+        button.setTitle("Continue", for: UIControl.State())
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor.black, for: UIControl.State())
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.layer.cornerRadius = 25
         
-        if indexPath.row == 0 {
-            randUser.removeAll()
-            let user = self.users.randomElement()!
-            self.randUser.append(user)
-            cell.textLabel?.text = user.name
-            cell.detailTextLabel?.text = user.email
-        }
-        return cell
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 72
-    }
+        button.addTarget(self, action: #selector(handleContinue), for: .touchUpInside)
+        
+        return button
+    }()
     
     
     var messagesController: MessagesController?
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    @objc func handleContinue() {
         dismiss(animated: true) {
             print("Dismiss completed")
-            let user = self.randUser[0]
-            self.messagesController?.showChatControllerForUser(user)
+            let match = self.randUser[0]
+            self.messagesController?.showChatControllerForUser(match)
         }
     }
+    
+    
+    
+    func setupMatchNameLabel() {
+        //need x, y, width, height constraints
+        matchNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        matchNameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        matchNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+    }
+    
+    func setupContinueButton() {
+        //need x, y, width, height constraints
+        ContinueButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        ContinueButton.topAnchor.constraint(equalTo: matchNameLabel.bottomAnchor, constant: 12).isActive = true
+        ContinueButton.widthAnchor.constraint(equalTo: matchNameLabel.widthAnchor).isActive = true
+        ContinueButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+
 }
+
