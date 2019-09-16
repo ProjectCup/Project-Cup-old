ab3a1a9d42c671277fef3b4f3258dfcc1d5c4d24
xyystartup <45159565+xyystartup@users.noreply.github.com>
2019-09-16 09:38:33 +0800

    Update MoodController.swift
    9/15 Update, delete mood.

--- ProjectCup/Controller/Mood/MoodController.swift
+++ ProjectCup/Controller/Mood/MoodController.swift
@@ -8,165 +8,175 @@
 
 import Foundation
 import UIKit
-
+import Firebase
 
 class MoodController: UIViewController{
-
-    var entries: [MoodEntry] = []
     let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .plain)
     let cellId = "cellId"
-
+    var journals = [MoodJournal]()
+    var journalsDictionary = [String: MoodJournal]()
+    
+    
     func setupTableViews() {
-
+        
         tableView.register(MoodEntryTableViewCell.self, forCellReuseIdentifier: cellId)
         view.addSubview(tableView)
-
     }
-
-    func createEntry(mood: MoodEntry.Mood, date: Date, moodStory: String?) {
-        let newEntry = MoodEntry(mood: mood, date: date, moodStory: moodStory)
-        entries.insert(newEntry, at: 0)
-        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
-    }
-
-    func updateEntry(mood: MoodEntry.Mood, date: Date, at index: Int, moodStory: String?) {
-        entries[index].mood = mood
-        entries[index].date = date
-        entries[index].moodStory = moodStory
-        tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
-    }
-
-    func deleteEntry(at index: Int) {
-        entries.remove(at: index)
-        tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
-    }
-
-    @objc func newmood() {
-        let addmoodController = AddMoodController()
-        addmoodController.mood = MoodEntry.Mood.none
-        addmoodController.date = Date()
-        addmoodController.hidesBottomBarWhenPushed = true
-        addmoodController.delegate = self
-        navigationController?.pushViewController(addmoodController, animated: true)
-    }
-//    @objc func savemood() {
-//
-//        let addmoodController = AddMoodController()
-//        let newMood: MoodEntry.Mood = addmoodController.mood
-//        let newDate: Date = addmoodController.date
-//        if addmoodController.isEditingEntry {
-//            guard let selectedIndexPath = tableView.indexPathForSelectedRow else {
-//                return
-//            }
-//            updateEntry(mood: newMood, date: newDate, at: selectedIndexPath.row)
-//        } else {
-//            createEntry(mood: newMood, date: newDate)
-//        }
-//        navigationController?.popViewController(animated: true)
-//
-//    }
-
-    //MARK: Override functions
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.delegate = self
         tableView.dataSource = self
-
-        let goodEntry = MoodEntry(mood: .good, date: Date(), moodStory: "1")
-        let badEntry = MoodEntry(mood: .bad, date: Date(), moodStory: "2")
-        let neutralEntry = MoodEntry(mood: .neutral, date: Date(), moodStory: "3")
-
-        entries = [goodEntry, badEntry, neutralEntry]
         tableView.reloadData()
-
+        
         //
         navigationItem.title = ("Mood")
         setNaviBarItems()
         setupTableViews()
-
+        observerJournal()
+        
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if let selectedIndexPath = tableView.indexPathForSelectedRow {
             tableView.deselectRow(at: selectedIndexPath, animated: true)
         }
     }
+    
+    func observerJournal() {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let ref = Database.database().reference().child("Journal").child(uid)
+        ref.observe(.childAdded, with: { (snapshot) in
+            let jnlId = snapshot.key
+//            if let journalId = snapshot.value as? [String: AnyObject] {
+//                let jnl = MoodJournal(dictionary: journalId)
+//                jnl.journalId = snapshot.key
+//                self.journals.append(jnl)
+//            }
+            
+            let journalReference = Database.database().reference().child("Journal").child(uid).child(jnlId)
+            
+            journalReference.observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    let journal = MoodJournal(dictionary: dictionary)
+                    journal.setValuesForKeys(dictionary)
+                    journal.journalId = jnlId
+                    self.journals.append(journal)
+//                    if let mood = journal.mood{
+//                        self.journalsDictionary[mood] = journal
+//                        self.journals = Array(self.journalsDictionary.values)
+//                    }
+                    self.timer?.invalidate()
+                    print("we just canceled our timer")
+                    
+                    self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.handleReloadTable), userInfo: nil, repeats: false)
+                    print("schedule a table reload in 0.1 sec")
+
+                }
+            }, withCancel: nil)
+        }, withCancel: nil)
+    }
+    
+    var timer: Timer?
+    
+    @objc func handleReloadTable() {
+//        self.journals = Array(self.journalsDictionary.values)
+
+        
+        //this will crash because of background thread, so lets call this on dispatch_async main thread
+        DispatchQueue.main.async(execute: {
+            self.tableView.reloadData()
+        })
+    }
+    
+    @objc func newmood() {
+        let addmoodController = AddMoodController()
+        addmoodController.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(addmoodController, animated: true)
+    }
+    
     private func setNaviBarItems() {
         //right bar item
-
+        
         let addmoodButton = UIButton(type: .system)
         addmoodButton.setImage(UIImage(named: "New chat"), for: .normal)
         addmoodButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
         addmoodButton.addTarget(self, action: #selector(newmood), for: .touchUpInside)
-
+        
         let addmoodItem = UIBarButtonItem(customView: addmoodButton)
         addmoodItem.customView?.widthAnchor.constraint(equalToConstant: 24).isActive = true
         addmoodItem.customView?.heightAnchor.constraint(equalToConstant: 24).isActive = true
         self.navigationItem.rightBarButtonItem = addmoodItem
-
     }
-
+    
+    func deleteEntry(at index: Int) {
+        journals.remove(at: index)
+        tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+    }
 }
 
 extension MoodController: UITableViewDataSource, UITableViewDelegate {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return entries.count
+        print(journals.count)
+        return journals.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! MoodEntryTableViewCell
-
-        let entry = entries[indexPath.row]
-        cell.configure(entry)
+        let journal = journals[indexPath.row]
+        cell.moodjournal = journal
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedEntry = entries[indexPath.row]
-        print("Selected mood was \(selectedEntry.mood.stringValue)")
-
+        let selectedEntry = journals[indexPath.row]
+        print("Selected mood was \(selectedEntry.mood)")
+        
         let addmoodController = AddMoodController()
+        addmoodController.textField.text = selectedEntry.journal
         addmoodController.mood = selectedEntry.mood
-        addmoodController.date = selectedEntry.date
-        addmoodController.textField.text = selectedEntry.moodStory
-        addmoodController.indexrow = indexPath.row
-        addmoodController.isEditingEntry = true
+        addmoodController.journalId = selectedEntry.journalId
+        addmoodController.dateinputField.text = selectedEntry.time
         addmoodController.hidesBottomBarWhenPushed = true
-        addmoodController.delegate = self
+        addmoodController.isEditingEntry = true
         navigationController?.pushViewController(addmoodController, animated: true)
     }
-
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         switch editingStyle {
         case .delete:
+            guard let uid = Auth.auth().currentUser?.uid else {
+                return
+            }
+            
+            let journal = journals[indexPath.row]
+            let ref = Database.database().reference().child("Journal").child(uid)
+            ref.observe(.childAdded, with: { (snapshot) in
+                let journalId = journal.journalId
+                let journalReference = Database.database().reference().child("Journal").child(uid).child(journalId!)
+                journalReference.removeValue(completionBlock: { (error, ref) in
+                    
+                    if error != nil {
+                        print("Failed to delete journal:", error!)
+                        return
+                    }
+                    self.attemptReloadOfTable()
+                })
+            }, withCancel: nil)
             deleteEntry(at: indexPath.row)
         default:
             break
         }
     }
-}
-
-extension MoodController: savemoodDelegate {
-    func savemood(mood: MoodEntry.Mood!, date: Date!, at: Int?, isEditingEntry: Bool!, moodStory: String?) {
+    
+    fileprivate func attemptReloadOfTable() {
+        self.timer?.invalidate()
         
-        let newMood: MoodEntry.Mood = mood
-        let newDate: Date = date
-        let newStory: String? = moodStory
-        if isEditingEntry == true {
-            guard let selectedIndexPath = tableView.indexPathForSelectedRow else {
-                return
-            }
-            updateEntry(mood: newMood, date: newDate, at: selectedIndexPath.row, moodStory: newStory)
-        } else {
-            createEntry(mood: newMood, date: newDate, moodStory: newStory)
-        }
-        navigationController?.popViewController(animated: true)
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.handleReloadTable), userInfo: nil, repeats: false)
     }
 }
-
-
-
